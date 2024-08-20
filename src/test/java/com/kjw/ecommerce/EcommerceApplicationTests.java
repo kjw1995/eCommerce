5c31bf30eb7f721d1e08a27137a2b1f8e2a848e2
kimjungwoo <wjdddn312@hellofunding.co.kr>
2024-08-20 17:53:51 +0900

    eCommerce Project Init!!

---
+++ src/test/java/com/kjw/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjw.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
