bd8bc0cf208e3998349819ca2b996617c03cc4a2
kimjungwoo <wjdddn312@hellofunding.co.kr>
2024-10-08 17:52:33 +0900

    회원가입 컨트롤러 및 로그인/회원가입 페이지 수정

---
+++ src/main/java/com/kjw/ecommerce/controller/join/JoinController.java
@@ -0,0 +1,14 @@
+package com.kjw.ecommerce.controller.join;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+
+    @PostMapping("/join")
+    public void joinMember() {
+
+    }
+
+}
